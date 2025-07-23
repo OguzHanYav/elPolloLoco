fe1dbf049ece90510064099831f6eb43e74ea7ac
OguzHanYav <hanouz34@hotmail.com>
2025-07-23 11:31:34 +0200

    Hurt  Animation

--- models/character.class.js
+++ models/character.class.js
@@ -4,7 +4,7 @@ class Character extends MovableObject {
   width = 120;
   world;
   speed = 10;
-  
+
   offset = {
     top: 100,
     bottom: 10,
@@ -39,7 +39,13 @@ class Character extends MovableObject {
     `img/2_character_pepe/5_dead/D-54.png`,
     `img/2_character_pepe/5_dead/D-55.png`,
     `img/2_character_pepe/5_dead/D-56.png`,
-    `img/2_character_pepe/5_dead/D-57.png`
+    `img/2_character_pepe/5_dead/D-57.png`,
+  ];
+
+  IMAGES_HURT = [
+    `img/2_character_pepe/4_hurt/H-41.png`,
+    `img/2_character_pepe/4_hurt/H-42.png`,
+    `img/2_character_pepe/4_hurt/H-43.png`,
   ];
 
   constructor() {
@@ -47,10 +53,10 @@ class Character extends MovableObject {
     this.loadImages(this.IMAGES_WALKING);
     this.loadImages(this.IMAGES_JUMPING);
     this.loadImages(this.IMAGES_DEAD);
+    this.loadImages(this.IMAGES_HURT);
     this.applyGravity();
     this.animate();
   }
-  
 
   animate() {
     //Camera moving
@@ -71,7 +77,10 @@ class Character extends MovableObject {
     setInterval(() => {
       if (this.isDead()) {
         this.playAnimation(this.IMAGES_DEAD);
-      } else if (this.isAboveGround()) {
+      } else if (this.isHurt()){
+        this.playAnimation(this.IMAGES_HURT);
+      } 
+      else if (this.isAboveGround()) {
         this.playAnimation(this.IMAGES_JUMPING);
       } else {
         if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
