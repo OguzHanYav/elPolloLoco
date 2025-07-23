f5aea1c2c3e2f9c5400df7bbd28f4b5d714cfd27
OguzHanYav <hanouz34@hotmail.com>
2025-07-23 11:15:36 +0200

    Dead Animation

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -50,7 +50,7 @@ class MovableObject {
       this.width - this.offset.left - this.offset.right,
       this.height - this.offset.top - this.offset.bottom
     );
-      ctx.stroke();
+    ctx.stroke();
   }
 
   isAboveGround() {
@@ -90,17 +90,20 @@ class MovableObject {
   }
 
   isColliding(mo) {
-    // return (
-    //   this.x + this.width > mo.x &&
-    //   this.y + this.height > mo.y &&
-    //   this.x < mo.x &&
-    //   this.y < mo.y + mo.height
-    // );
     return (
       this.x + this.width - this.offset.right > mo.x + mo.offset.left &&
       this.y + this.height - this.offset.bottom > mo.y + mo.offset.top &&
       this.x + this.offset.left < mo.x + mo.width - mo.offset.right &&
       this.y + this.offset.top < mo.y + mo.height - mo.offset.bottom
     );
+  }
+  hit() {
+    this.energy -= 5;
+    if (this.energy < 0) {
+      this.energy = 0;
+    }
+  }
+  isDead(){
+    return this.energy == 0;
   }
 }
