a388220eeb5de62520458a54636bc95dabbadd95
OguzHanYav <hanouz34@hotmail.com>
2025-09-11 17:06:53 +0200

    Statusbar posion fixed

--- models/world.class.js
+++ models/world.class.js
@@ -35,7 +35,9 @@ class World {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.translate(this.camera_x, 0);
     this.addObjectsToMap(this.level.backgroundObjects);
+    this.ctx.translate(-this.camera_x, 0);
     this.addToMap(this.statusBar);
+    this.ctx.translate(this.camera_x, 0);
     this.addToMap(this.character);
     this.addObjectsToMap(this.level.clouds);
     this.addObjectsToMap(this.level.enemies);
