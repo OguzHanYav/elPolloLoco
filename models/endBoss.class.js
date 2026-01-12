bd477c1fe95980bf82f9cfcd7e5859ae25503033
OguzHanYav <hanouz34@hotmail.com>
2026-01-12 10:38:38 +0300

    JSDoc endboss.class.js

--- models/endBoss.class.js
+++ models/endBoss.class.js
@@ -3,41 +3,64 @@
  * @extends MovableObject
  */
 class EndBoss extends MovableObject {
+  height = 450;
+  width = 250;
+  energy = 100;
+  isEndboss = true;
+  deadAnimationFinished = false;
+  isWalking = false;
+  isAttacking = false;
+  attackTriggered = false;
+  walkDistance = 800;
+  animationInterval = 150;
+  lastAnimationTime = 0;
 
   IMAGES_ALERT = [
-    "img/4_enemie_boss_chicken/2_alert/G5.png",
-    "img/4_enemie_boss_chicken/2_alert/G6.png",
-    "img/4_enemie_boss_chicken/2_alert/G7.png"
-  ];
-
-  IMAGES_WALKING = [
-    "img/4_enemie_boss_chicken/1_walk/G1.png",
-    "img/4_enemie_boss_chicken/1_walk/G2.png",
-    "img/4_enemie_boss_chicken/1_walk/G3.png",
-    "img/4_enemie_boss_chicken/1_walk/G4.png"
-  ];
-
-  IMAGES_ATTACK = [
-    "img/4_enemie_boss_chicken/3_attack/G13.png",
-    "img/4_enemie_boss_chicken/3_attack/G14.png",
-    "img/4_enemie_boss_chicken/3_attack/G15.png"
-  ];
-
-  IMAGES_HURT = [
-    "img/4_enemie_boss_chicken/4_hurt/G21.png",
-    "img/4_enemie_boss_chicken/4_hurt/G22.png"
+    `img/4_enemie_boss_chicken/2_alert/G5.png`,
+    `img/4_enemie_boss_chicken/2_alert/G6.png`,
+    `img/4_enemie_boss_chicken/2_alert/G7.png`,
+    `img/4_enemie_boss_chicken/2_alert/G8.png`,
+    `img/4_enemie_boss_chicken/2_alert/G9.png`,
+    `img/4_enemie_boss_chicken/2_alert/G10.png`,
+    `img/4_enemie_boss_chicken/2_alert/G11.png`,
+    `img/4_enemie_boss_chicken/2_alert/G12.png`,
   ];
 
   IMAGES_DEAD = [
-    "img/4_enemie_boss_chicken/5_dead/G24.png",
-    "img/4_enemie_boss_chicken/5_dead/G25.png",
-    "img/4_enemie_boss_chicken/5_dead/G26.png"
+    `img/4_enemie_boss_chicken/5_dead/G24.png`,
+    `img/4_enemie_boss_chicken/5_dead/G25.png`,
+    `img/4_enemie_boss_chicken/5_dead/G26.png`,
   ];
+
+  IMAGES_HURT = [
+    `img/4_enemie_boss_chicken/4_hurt/G21.png`,
+    `img/4_enemie_boss_chicken/4_hurt/G22.png`,
+    `img/4_enemie_boss_chicken/4_hurt/G23.png`,
+  ];
+
+  IMAGES_WALKING = [
+    `img/4_enemie_boss_chicken/1_walk/G1.png`,
+    `img/4_enemie_boss_chicken/1_walk/G2.png`,
+    `img/4_enemie_boss_chicken/1_walk/G3.png`,
+    `img/4_enemie_boss_chicken/1_walk/G4.png`,
+  ];
+
+  IMAGES_ATTACK = [
+    `img/4_enemie_boss_chicken/3_attack/G13.png`,
+    `img/4_enemie_boss_chicken/3_attack/G14.png`,
+    `img/4_enemie_boss_chicken/3_attack/G15.png`,
+    `img/4_enemie_boss_chicken/3_attack/G16.png`,
+    `img/4_enemie_boss_chicken/3_attack/G17.png`,
+    `img/4_enemie_boss_chicken/3_attack/G18.png`,
+    `img/4_enemie_boss_chicken/3_attack/G19.png`,
+    `img/4_enemie_boss_chicken/3_attack/G20.png`,
+  ];
+
   /**
    * Creates the end boss and loads all required images.
    */
   constructor() {
-    super().loadImage("img/4_enemie_boss_chicken/2_alert/G5.png");
+    super();
     this.loadImage(this.IMAGES_ALERT[0]);
     this.loadImages(this.IMAGES_ALERT);
     this.loadImages(this.IMAGES_DEAD);
@@ -63,22 +86,18 @@ class EndBoss extends MovableObject {
     if (now - this.lastAnimationTime > this.animationInterval) {
       this.lastAnimationTime = now;
       if (!this.world || !this.world.character) return;
-
       this.updateState();
-
-      if (this.isDeadBoss) {
-        this.handleDeathAnimation();
-      } else if (this.isHurt) {
-        this.playAnimation(this.IMAGES_HURT);
-      } else if (this.isAttacking) {
+      if (this.isDeadBoss) this.handleDeathAnimation();
+      else if (this.isHurt) this.playAnimation(this.IMAGES_HURT);
+      else if (this.isAttacking) {
         this.playAnimation(this.IMAGES_ATTACK);
         this.handleAttackMovement();
-      } else if (this.isWalking) {
+      }
+      else if (this.isWalking) {
         this.playAnimation(this.IMAGES_WALKING);
         this.handleWalking();
-      } else {
-        this.playAnimation(this.IMAGES_ALERT);
       }
+      else this.playAnimation(this.IMAGES_ALERT);
     }
   }
 
@@ -87,22 +106,18 @@ class EndBoss extends MovableObject {
    */
   updateState() {
     if (this.isDeadBoss) return;
-
     const charDistance = this.world.character.x - this.x;
     this.isWalking = !this.attackTriggered && Math.abs(charDistance) < this.walkDistance;
     this.isAttacking = this.attackTriggered;
-
     if (this.isAttacking) this.isWalking = false;
-
     this.otherDirection = this.world.character.x > this.x;
   }
 
   /**
-   * Handles walking movement towards the player.
+   * Handles walking movement towards the character.
    */
   handleWalking() {
     if (!this.isWalking) return;
-
     const char = this.world.character;
     this.x += char.x > this.x ? 5 : -5;
     this.otherDirection = char.x > this.x;
@@ -116,7 +131,7 @@ class EndBoss extends MovableObject {
   }
 
   /**
-   * Handles movement behavior while attacking the player.
+   * Handles movement while the end boss is attacking.
    */
   handleAttackMovement() {
     const char = this.world.character;
@@ -132,11 +147,11 @@ class EndBoss extends MovableObject {
   }
 
   /**
-   * Plays the death animation and marks it as finished.
+   * Plays and finalizes the death animation.
    */
   handleDeathAnimation() {
     if (!this.deadAnimationFinished) {
-      this.playAnimation(this.IMAGES_DEAD);
+      this.playAnimation(this.IMAGES_DEAD, 300);
 
       if (this.currentImage >= this.IMAGES_DEAD.length - 1) {
         this.deadAnimationFinished = true;
@@ -151,22 +166,19 @@ class EndBoss extends MovableObject {
    */
   hit() {
     if (this.isDeadBoss) return;
-
     this.energy -= 20;
-
-    if (this.energy <= 0) {
+    if (this.energy < 0) {
       this.energy = 0;
       this.isDeadBoss = true;
       return;
     }
-
     this.isHurt = true;
     setTimeout(() => (this.isHurt = false), 300);
   }
 
   /**
    * Checks whether the end boss is dead.
-   * @returns {boolean} True if energy is zero.
+   * @returns {boolean} True if energy is zero or below.
    */
   isDead() {
     return this.energy <= 0;
